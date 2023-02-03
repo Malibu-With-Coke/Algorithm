5763f919e687147ef97032504fe1ed77e8a17742
Malibu-With-Coke <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-02-03 19:29:54 +0900

    [Gold V] Title: 퇴사 2, Time: 3592 ms, Memory: 398996 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+
+d = {}
+for i in range(1,n+1):
+    during_days, payment = map(int, sys.stdin.readline().split())
+    d[i] = [during_days, payment]
+
+dp = [0 for i in range(n+2)]    # 0 ~ n+1
+
+max_num = 0
+
+for i in range(n, 0, -1):
+    during_days, payment = d[i]
+
+    if during_days + i > n+1:
+        dp[i] = dp[i+1]
+        continue
+    
+    dp[i] = max(dp[i+1], dp[i+during_days]+payment)
+    if dp[i] > max_num:
+        max_num = dp[i]
+
+print(max_num)
