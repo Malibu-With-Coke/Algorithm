81fb3d60cded80e079b7484d24685749ca34fa63
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-02-07 21:20:55 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+def backTracking(num,idx):
+    if num == m:
+        for i in arr:
+            print(i, end=" ")
+        print()
+    else:
+        for i in range(idx,n+1):
+            arr.append(i)
+            backTracking(num+1, i+1)
+            arr.pop()
+
+arr = []
+backTracking(0,1)
