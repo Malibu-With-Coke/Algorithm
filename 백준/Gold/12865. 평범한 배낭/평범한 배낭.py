d1cdc758b11c9f4592bcacb534843d08267a80c4
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-02-13 18:31:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 5620 ms, Memory: 376676 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,20 @@
+n, k = map(int, input().split())
+
+goods = [tuple(map(int, input().split())) for _ in range(n)]
+
+goods.sort(key = lambda x: (x[1]/x[0], x[0]))
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        weight = goods[i-1][0]
+        value = goods[i-1][1]
+        if weight <= j:
+            if value + dp[i-1][j-weight] > dp[i-1][j]:
+                dp[i][j] = value + dp[i-1][j-weight]
+            else:
+                dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[i][j])
