96e414e31a135df0ddb4619b716d09426df55aae
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-02-15 15:33:02 +0900

    [Gold IV] Title: 플로이드, Time: 708 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(input())
+m = int(input())
+
+INF = 99999999
+
+cities = [[INF for _ in range(n)] for _ in range(n)]
+
+for i in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if cities[a-1][b-1] > c:
+        cities[a-1][b-1] = c
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            cities[i][j] = min(cities[i][j], cities[i][k] + cities[k][j])
+
+for i in range(n):
+    for j in range(n):
+        if cities[i][j] == INF:
+            print(0, end=" ")
+        else:
+            print(cities[i][j], end=" ")
+    print()
