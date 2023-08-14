657a8933948fc3248b9c50c3d8ce1c461a7b7a38
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-08-15 00:08:43 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 2856 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+ans = int(10e9)
+
+for a_left in range(n-3):
+    for a_right in range(a_left+3, n):
+        b_left = a_left + 1
+        b_right = a_right - 1
+        a_sum = arr[a_left] + arr[a_right]
+
+        while b_left < b_right:
+            if ans > abs(a_sum - (arr[b_left] + arr[b_right])):
+                ans = abs(a_sum - (arr[b_left] + arr[b_right]))
+
+            if a_sum > arr[b_left] + arr[b_right]:
+                b_left += 1
+            elif a_sum < arr[b_left] + arr[b_right]:
+                b_right -= 1
+            else:
+                ans = 0
+                break
+
+        if ans == 0:
+            break
+
+print(ans)
