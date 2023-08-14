d6124133a1bc83f451b4e5315599c3072d0bc1ca
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2023-08-14 21:32:21 +0900

    [Gold II] Title: 구간 자르기, Time: 1708 ms, Memory: 122304 KB -BaekjoonHub

---
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+n, target = map(int, input().split())
+arr = [0 for _ in range(1000001)]
+start = int(10e9)
+end = 0
+
+for _ in range(n):
+    s, e = map(int, input().split())
+    if start > s: start = s
+    if end < e: end = e
+    for i in range(s, e):
+        arr[i] += 1
+
+sum_arr = 0
+start_idx = 0
+end_idx = 0
+flag = False
+
+for start_idx in range(0, end+1):
+    while sum_arr < target and end_idx <= end:
+        sum_arr += arr[end_idx]
+        end_idx += 1
+
+    if sum_arr == target:
+        flag = True
+        break
+
+    sum_arr -= arr[start_idx]
+
+if flag:
+    print(start_idx, end_idx)
+else:
+    print(0, 0)
