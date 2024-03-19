41a7329f6b67ea284674115f0ca853c6b339f797
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2024-03-19 17:53:15 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 2768 ms, Memory: 69704 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -4,14 +4,8 @@ answer = [[' ' for _ in range(num)] for __ in range(num)]
 
 # print(answer)
 def star(n,x,y):
-    if n == 3:
-        for i in range(x,x+3):
-            for j in range(y,y+3):
-                if i == x+1 and j == y+1:
-                    continue
-                else:
-                    answer[i][j] = '*'
-                    # print(answer)
+    if n == 1:
+        answer[x][y] = '*'
                     
     else:
         n = n//3
@@ -25,7 +19,6 @@ def star(n,x,y):
         star(n, x+2*n, y+2*n)
 
 star(num, 0, 0)
-# print(answer)
 for i in range(num):
     for j in range(num):
         print(answer[i][j],end="")
