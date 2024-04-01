8512c5cf3a378ee9388be2bff1c709549d9906d9
최정겸 <108716616+Malibu-With-Coke@users.noreply.github.com>
2024-04-01 16:21:00 +0900

    [Gold II] Title: 보석 도둑, Time: 1360 ms, Memory: 106592 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -8,18 +8,16 @@ num_diamond, num_bag = map(int, input().split())
 diamond = [list(map(int, input().split())) for _ in range(num_diamond)]  # 무게, 가격
 bags = [int(input()) for _ in range(num_bag)]
 
-diamond.sort()
+diamond.sort(reverse=True)
 bags.sort()
 
-diamond_idx = 0
 can_pack = []
 sum_weight = 0
 for bag in bags:
 
-    while diamond_idx < num_diamond and diamond[diamond_idx][0] <= bag:
-        heapq.heappush(can_pack, -diamond[diamond_idx][1])
-        diamond_idx += 1
-
+    while diamond and diamond[-1][0] <= bag:
+        heapq.heappush(can_pack, -diamond[-1][1])
+        diamond.pop()
     if can_pack:
         sum_weight -= heapq.heappop(can_pack)
 
